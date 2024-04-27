2e627b372b2d8bf4570ead2897d9bd5e265d0887
Wamur <92934197+Wamur@users.noreply.github.com>
2024-04-27 10:02:44 -0400

    Add files via upload

---
+++ Scripts/xbox.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+base_url = "https://www.trueachievements.com/allgames.aspx?page="
+
+all_games = []
+
+for page_num in range(1, 111):
+    url = base_url + str(page_num)
+    
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, "html.parser")
+    
+    for row in soup.find_all('tr')[1:]:
+        game = {}
+        cells = row.find_all('td')
+        game['name'] = cells[1].text.strip()
+        game['score'] = cells[2].text.strip()
+        game['ratio'] = cells[3].text.strip()
+        game['gamers'] = cells[4].text.strip()
+        game['completion_percentage'] = cells[5].text.strip()
+        game['completion_time'] = cells[6].text.strip()
+        game['rating'] = cells[7].text.strip()
+        all_games.append(game)
+
+df = pd.DataFrame(all_games)
+df.to_csv('xbox.csv', index=False)
